6dd742ceb01bf5cd5d2b3d43d2cae30ced31adeb
Sven Brauch <mail@svenbrauch.de>
2016-06-24 23:10:26 +0200

    add some comments on what this is for

--- exec.c
+++ exec.c
@@ -17,6 +17,29 @@
  * Boston, MA 02110-1301, USA.
  */
 
+/**
+This library is intended to be used together with the AppImage distribution mechanism.
+Place the library somewhere in your AppImage and point LD_PRELOAD to it
+before launching your application.
+
+Whenever your application invokes a child process through execv() or execve(),
+this wrapper will intercept the call and see if the child process lies
+outside of the bundled appdir. If it does, the wrapper will attempt to undo
+any changes done to environment variables before launching the process,
+since you probably did not intend to launch it with e.g. the LD_LIBRARY_PATH
+you previously set for your application.
+
+To perform this operation, you have to set the following environment variables:
+  $APPDIR -- path of the AppDir you are launching your application from. If this
+             is not present, the wrapper will do nothing.
+
+For each environment variable you want restored, where {VAR} is the name of the environment
+variable (e.g. "PATH"):
+  $APPIMAGE_ORIGINAL_{VAR} -- original value of the environment variable
+  $APPIMAGE_STARTUP_{VAR} -- value of the variable when you were starting up
+                             your application
+*/
+
 #define _GNU_SOURCE
 
 #include <unistd.h>
@@ -25,10 +48,6 @@
 #include <string.h>
 #include <malloc.h>
 #include <stdlib.h>
-
-//       implement posix_spawn, fork
-//       backup vars in C file
-//       read vars from text file from AppRun
 
 typedef ssize_t (*execve_func_t)(const char* filename, char* const argv[], char* const envp[]);
 static execve_func_t old_execve = NULL;
