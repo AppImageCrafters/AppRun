89320487af346b4c82ed97bc54a69a8dec0f131c
Alexis Lopez Zubieta <contact@azubieta.net>
2020-04-07 19:49:06 -0500

    complete setup_runtime_environment

--- src/apprun/runtime_environmet.c
+++ src/apprun/runtime_environmet.c
@@ -30,19 +30,45 @@
 
 #include "runtime_environmet.h"
 #include "../common/file_utils.h"
+#include "../common/shell_utils.h"
+#include "../hooks/environment.h"
 
 char* get_env_file_path(const char* appdir) {
-    char* path = calloc(strlen(appdir) + strlen("/.env"), sizeof(char));
+    char* path = calloc(strlen(appdir) + strlen("/.env") + 1, sizeof(char));
     strcat(path, appdir);
     strcat(path, "/.env");
 
     return path;
 }
 
+void apprun_setenv_prefixed(const char* prefix, const char* name, const char* value) {
+    unsigned prefixed_name_size = strlen(prefix) + strlen(name) + 1;
+    char* prefixed_name = calloc(prefixed_name_size, sizeof(char));
+    strcat(prefixed_name, prefix);
+    strcat(prefixed_name, name);
+
+    if (value)
+        setenv(prefixed_name, value, 1);
+
+    free(prefixed_name);
+}
+
 void setup_runtime_environment(char* appdir) {
     char* env_file_path = get_env_file_path(appdir);
     char** env = apprun_file_read_lines(env_file_path);
 
-    for (char** itr = env; *itr != NULL; itr++)
-        printf("%s\n", *itr);
+    for (char** itr = env; *itr != NULL; itr++) {
+        char* name = apprun_env_str_entry_extract_name(*itr);
+        char* value = apprun_env_str_entry_extract_value(*itr);
+        char* expanded_value = apprun_shell_expand_variables(value);
+
+        char* original_value = getenv(name);
+        setenv(name, expanded_value, 1);
+        apprun_setenv_prefixed(APPRUN_ENV_ORIG_PREFIX, name, original_value);
+        apprun_setenv_prefixed(APPRUN_ENV_STARTUP_PREFIX, name, expanded_value);
+
+        free(expanded_value);
+        free(value);
+        free(name);
+    }
 }
