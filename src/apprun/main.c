20a072fde25c3f3bf54e8ede03ce5fc601bc507a
Alexis Lopez Zubieta <contact@azubieta.net>
2021-02-25 10:35:17 -0600

    move main function to the bottom of the file

--- src/apprun/main.c
+++ src/apprun/main.c
@@ -47,25 +47,155 @@
         exit(1);                                    \
     } while(0);
 
-char* resolve_apprun_path();
+char* add_appdir_libc_prefix_to_path(const char* appdir, const char* path) {
+    char* appdir_interpreter_path = calloc(sizeof(char), PATH_MAX);
+    memset(appdir_interpreter_path, 0, PATH_MAX);
 
-char* find_legacy_env_file(char* apprun_path);
+    appdir_interpreter_path = strcat(appdir_interpreter_path, appdir);
+    appdir_interpreter_path = strcat(appdir_interpreter_path, "/opt/libc");
+    appdir_interpreter_path = strcat(appdir_interpreter_path, path);
 
-char* build_env_file_path(char* apprun_path, unsigned long i);
+    return appdir_interpreter_path;
+}
 
-char* resolve_appdir_path(const char* root_env_file_path);
+void export_binary(const char* filename) {
+    char* appimage_uuid = getenv("APPIMAGE_UUID");
+    if (appimage_uuid == NULL)
+        die("Unable to export runtime binaries, missing APPIMAGE_UUID in runtime environment")
 
-char* find_module_env_file(char* apprun_path);
+    char target_path[PATH_MAX] = {0x0};
+    strcat(target_path, "/tmp/appimage-");
+    strcat(target_path, appimage_uuid);
+    strcat(target_path, "-");
+    strcat(target_path, strrchr(filename, '/') + 1);
 
-void launch();
+    if (access(target_path, F_OK) == -1) {
+        apprun_file_copy(filename, target_path);
 
-char* resolve_origin(const char* apprun_path);
+        // Copy permissions and ownership
+        struct stat fst;
+        stat(filename, &fst);
+        chown(target_path, fst.st_uid, fst.st_gid);
+        chmod(target_path, fst.st_mode);
+    }
+}
 
-void export_binaries(char* binaries);
+void export_binaries(char* binaries) {
+    char* token = strtok(binaries, ":");
+    while (token != NULL) {
+        export_binary(token);
+        token = strtok(NULL, ":");
+    }
+}
 
-void export_binary(const char* filename);
+char* resolve_origin(const char* apprun_path) {
+    char* origin_path_end = strrchr(apprun_path, '/');
+    char* origin_path = strndup(apprun_path, origin_path_end - apprun_path);
 
-char* add_appdir_libc_prefix_to_path(const char* appdir, const char* path);
+    return origin_path;
+}
+
+char* resolve_appdir_path(const char* root_env_file_path) {
+    char* appdir = getenv("APPDIR");
+    if (appdir == NULL) {
+        if (root_env_file_path != NULL) {
+            char* idx = strrchr(root_env_file_path, '/');
+            appdir = strndup(root_env_file_path, idx - root_env_file_path);
+        } else {
+            die("Could not resolve APPDIR\n");
+        }
+    }
+    return appdir;
+}
+
+char* find_module_env_file(char* apprun_path) {
+    const char apprun_env_extension[] = ".env";
+    const unsigned long apprun_env_extension_len = strlen(apprun_env_extension);
+
+    const unsigned long possible_path_len = strlen(apprun_path) + apprun_env_extension_len;
+    char* possible_path = malloc(possible_path_len);
+    memset(possible_path, 0, possible_path_len);
+
+    strcat(possible_path, apprun_path);
+    strncat(possible_path, apprun_env_extension, apprun_env_extension_len);
+
+#ifdef DEBUG
+    fprintf(stderr, "APPRUN_DEBUG: Looking for %s file at: %s\n", apprun_env_extension, possible_path);
+#endif
+    if (access(possible_path, F_OK) == 0)
+        return possible_path;
+    else
+        free(possible_path);
+
+
+    return NULL;
+}
+
+char* resolve_apprun_path() {
+    char* apprun_path = realpath("/proc/self/exe", NULL);
+
+#ifdef DEBUG
+    fprintf(stderr, "APPRUN_DEBUG: APPRUN_PATH=%s\n", apprun_path);
+#endif
+    return apprun_path;
+}
+
+char* build_env_file_path(char* apprun_path, unsigned long i) {
+    char env_file_name[] = ".env";
+    unsigned long env_file_name_len = 4;
+
+    unsigned possible_path_len = i + env_file_name_len + 2 /*ZERO TERMINATION*/;
+    char* possible_path = calloc(possible_path_len, sizeof(char));
+    memset(possible_path, 0, possible_path_len);
+    strncat(possible_path, apprun_path, i + 1);
+    strncat(possible_path, env_file_name, env_file_name_len);
+
+    return possible_path;
+}
+
+char* find_legacy_env_file(char* apprun_path) {
+    char* slash_idx = strrchr(apprun_path, '/');
+
+    if (slash_idx != NULL) {
+        char* possible_path = build_env_file_path(apprun_path, slash_idx - apprun_path);
+#ifdef DEBUG
+        fprintf(stderr, "APPRUN_DEBUG: Looking for .env file at: %s\n", possible_path);
+#endif
+        if (access(possible_path, F_OK) == 0) {
+            return possible_path;
+        } else {
+            free(possible_path);
+        }
+    }
+
+    return NULL;
+}
+
+void launch(char* interpreter_path, char* library_path) {
+    char* exec_path = getenv("EXEC_PATH");
+    char* exec_args = getenv("EXEC_ARGS");
+
+    char** user_args = apprun_shell_split_arguments(exec_args);
+    unsigned user_args_len = apprun_string_list_len(user_args);
+    char** argv = calloc(user_args_len + 5, sizeof(char*));
+    argv[0] = interpreter_path;
+    argv[1] = "--library-path";
+    argv[2] = library_path;
+    argv[3] = exec_path;
+
+    for (int i = 0; i < user_args_len; i++)
+        argv[i + 4] = user_args[i];
+
+#ifdef DEBUG
+    fprintf(stderr, "APPRUN_DEBUG: executing ");
+    for (char** itr = argv; itr != NULL && *itr != NULL; itr++)
+        fprintf(stderr, "\"%s\" ", *itr);
+    fprintf(stderr, "\n");
+#endif
+
+    int ret = execv(interpreter_path, argv);
+    fprintf(stderr, "APPRUN_ERROR: %s", strerror(errno));
+}
 
 int main(int argc, char* argv[]) {
     char* apprun_path = resolve_apprun_path();
@@ -128,154 +258,3 @@ int main(int argc, char* argv[]) {
     return 1;
 }
 
-char* add_appdir_libc_prefix_to_path(const char* appdir, const char* path) {
-    char* appdir_interpreter_path = calloc(sizeof(char), PATH_MAX);
-    memset(appdir_interpreter_path, 0, PATH_MAX);
-
-    appdir_interpreter_path = strcat(appdir_interpreter_path, appdir);
-    appdir_interpreter_path = strcat(appdir_interpreter_path, "/opt/libc");
-    appdir_interpreter_path = strcat(appdir_interpreter_path, path);
-
-    return appdir_interpreter_path;
-}
-
-void export_binaries(char* binaries) {
-    char* token = strtok(binaries, ":");
-    while (token != NULL) {
-        export_binary(token);
-        token = strtok(NULL, ":");
-    }
-}
-
-void export_binary(const char* filename) {
-    char* appimage_uuid = getenv("APPIMAGE_UUID");
-    if (appimage_uuid == NULL)
-        die("Unable to export runtime binaries, missing APPIMAGE_UUID in runtime environment")
-
-    char target_path[PATH_MAX] = {0x0};
-    strcat(target_path, "/tmp/appimage-");
-    strcat(target_path, appimage_uuid);
-    strcat(target_path, "-");
-    strcat(target_path, strrchr(filename, '/') + 1);
-
-    if (access(target_path, F_OK) == -1) {
-        apprun_file_copy(filename, target_path);
-
-        // Copy permissions and ownership
-        struct stat fst;
-        stat(filename, &fst);
-        chown(target_path, fst.st_uid, fst.st_gid);
-        chmod(target_path, fst.st_mode);
-    }
-}
-
-char* resolve_origin(const char* apprun_path) {
-    char* origin_path_end = strrchr(apprun_path, '/');
-    char* origin_path = strndup(apprun_path, origin_path_end - apprun_path);
-
-    return origin_path;
-}
-
-char* resolve_appdir_path(const char* root_env_file_path) {
-    char* appdir = getenv("APPDIR");
-    if (appdir == NULL) {
-        if (root_env_file_path != NULL) {
-            char* idx = strrchr(root_env_file_path, '/');
-            appdir = strndup(root_env_file_path, idx - root_env_file_path);
-        } else {
-            die("Could not resolve APPDIR\n");
-        }
-    }
-    return appdir;
-}
-
-char* find_module_env_file(char* apprun_path) {
-    const char apprun_env_extension[] = ".env";
-    const unsigned long apprun_env_extension_len = strlen(apprun_env_extension);
-
-    const unsigned long possible_path_len = strlen(apprun_path) + apprun_env_extension_len;
-    char* possible_path = malloc(possible_path_len);
-    memset(possible_path, 0, possible_path_len);
-
-    strcat(possible_path, apprun_path);
-    strncat(possible_path, apprun_env_extension, apprun_env_extension_len);
-
-#ifdef DEBUG
-    fprintf(stderr, "APPRUN_DEBUG: Looking for %s file at: %s\n", apprun_env_extension, possible_path);
-#endif
-    if (access(possible_path, F_OK) == 0)
-        return possible_path;
-    else
-        free(possible_path);
-
-
-    return NULL;
-}
-
-char* resolve_apprun_path() {
-    char* apprun_path = realpath("/proc/self/exe", NULL);
-
-#ifdef DEBUG
-    fprintf(stderr, "APPRUN_DEBUG: APPRUN_PATH=%s\n", apprun_path);
-#endif
-    return apprun_path;
-}
-
-char* find_legacy_env_file(char* apprun_path) {
-    char* slash_idx = strrchr(apprun_path, '/');
-
-    if (slash_idx != NULL) {
-        char* possible_path = build_env_file_path(apprun_path, slash_idx - apprun_path);
-#ifdef DEBUG
-        fprintf(stderr, "APPRUN_DEBUG: Looking for .env file at: %s\n", possible_path);
-#endif
-        if (access(possible_path, F_OK) == 0) {
-            return possible_path;
-        } else {
-            free(possible_path);
-        }
-    }
-
-    return NULL;
-}
-
-
-char* build_env_file_path(char* apprun_path, unsigned long i) {
-    char env_file_name[] = ".env";
-    unsigned long env_file_name_len = 4;
-
-    unsigned possible_path_len = i + env_file_name_len + 2 /*ZERO TERMINATION*/;
-    char* possible_path = calloc(possible_path_len, sizeof(char));
-    memset(possible_path, 0, possible_path_len);
-    strncat(possible_path, apprun_path, i + 1);
-    strncat(possible_path, env_file_name, env_file_name_len);
-
-    return possible_path;
-}
-
-
-void launch(char* interpreter_path, char* library_path) {
-    char* exec_path = getenv("EXEC_PATH");
-    char* exec_args = getenv("EXEC_ARGS");
-
-    char** user_args = apprun_shell_split_arguments(exec_args);
-    unsigned user_args_len = apprun_string_list_len(user_args);
-    char** argv = calloc(user_args_len + 5, sizeof(char*));
-    argv[0] = interpreter_path;
-    argv[1] = "--library-path";
-    argv[2] = library_path;
-    argv[3] = exec_path;
-
-    for (int i = 0; i < user_args_len; i++)
-        argv[i + 4] = user_args[i];
-
-#ifdef DEBUG
-    fprintf(stderr, "APPRUN_DEBUG: executing ");
-    for (char** itr = argv; itr != NULL && *itr != NULL; itr++)
-        fprintf(stderr, "\"%s\" ", *itr);
-    fprintf(stderr, "\n");
-#endif
-
-    int ret = execv(interpreter_path, argv);
-    fprintf(stderr, "APPRUN_ERROR: %s", strerror(errno));
-}
