12acd43bf37ea29a50d10a8029fa532d2058c843
Alexis Lopez Zubieta <contact@azubieta.net>
2022-02-22 17:50:37 -0600

    ref: move apprun_set_original_workdir_env to exec_utils

--- src/hooks/exec_utils.h
+++ src/hooks/exec_utils.h
@@ -42,4 +42,5 @@ void apprun_print_exec_args(const char* filename, char* const* argv, char* const
 
 apprun_exec_args_t* apprun_duplicate_exec_args(const char* filename, char* const* argv);
 
+char **apprun_set_original_workdir_env(char *const *envp);
 #endif //APPDIR_RUMTIME_SHARED_H
