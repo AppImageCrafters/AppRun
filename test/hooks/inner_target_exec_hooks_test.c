e440e6d6a1d08f790e962375efe587edd0e6dac5
Alexis Lopez Zubieta <contact@azubieta.net>
2022-02-23 11:21:19 -0600

    ref: redefine exec_hooks_test as inner_target_exec_hooks_test

---
+++ test/hooks/inner_target_exec_hooks_test.c
@@ -0,0 +1,49 @@
+/**************************************************************************
+ *
+ * Copyright (c) 2020 Alexis Lopez Zubieta <contact@azubieta.net>
+ *
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ **************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <linux/limits.h>
+
+#include "../common/tests_shared.h"
+
+
+int main(int argc, char **argv) {
+    set_private_env("APPDIR", APPDIR_MOCK);
+    set_private_env("LD_PRELOAD", HOOKS_LIB_PATH);
+    set_private_env("APPRUN_RUNTIME", APPDIR_MOCK "/runtime/compat");
+    set_private_env("APPRUN_PATH_MAPPINGS", MAPPED_APPDIR_PATH":"APPDIR_MOCK";");
+
+    chdir(APPDIR_MOCK);
+
+    char *args[2] = {0x0};
+    args[0] = INNER_TARGET;
+
+    return execv(INNER_TARGET, args);;
+}
+
