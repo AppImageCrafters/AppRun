4c1a58fe84383928ecea72861203d4c1a26b22bd
Alexis Lopez Zubieta <contact@azubieta.net>
2022-02-24 14:23:53 -0600

    add script to build all docker images

---
+++ tooling/build-docker-images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(realpath "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
+while IFS= read -r -d '' dockerfile; do
+  TARGET_ARCH=$(basename "$dockerfile" | cut -f 2 -d .)
+  TARGET_IMAGE_NAME="appimagecrafters/apprun-build-env-$TARGET_ARCH"
+
+  echo "Building $TARGET_IMAGE_NAME"
+  docker build "$SCRIPT_DIR" -f "$dockerfile" -t "$TARGET_IMAGE_NAME"
+done < <(find "$SCRIPT_DIR" -name 'Dockerfile.*' -print0)
