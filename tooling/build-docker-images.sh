9a2b6a7c0097156505eb287f7e1c9d816b176ad7
Alexis Lopez Zubieta <contact@azubieta.net>
2022-02-24 23:41:44 -0600

    Allow uploading docker images after build

--- tooling/build-docker-images.sh
+++ tooling/build-docker-images.sh
@@ -9,4 +9,8 @@ while IFS= read -r -d '' dockerfile; do
 
   echo "Building $TARGET_IMAGE_NAME"
   docker build "$SCRIPT_DIR" -f "$dockerfile" -t "$TARGET_IMAGE_NAME"
+
+  if [[ -n "$UPLOAD_IMAGES" ]]; then
+    docker push "$TARGET_IMAGE_NAME"
+  fi
 done < <(find "$SCRIPT_DIR" -name 'Dockerfile.*' -print0)
